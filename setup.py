c7f3a278e8a324d85274378a43d72f4e6367b1b7
pwilczynskiclearcode <p.wilczynski@clearcode.cc>
2013-08-30 18:42:06 +0200

    Add licence information

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2013 by Clearcode <http://clearcode.cc>
+# and associates (see AUTHORS).
+#
+# This file is part of pytest_sauce.
+#
+# Pytest_sauce is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pytest_sauce is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pytest_sauce. If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import re
 from setuptools import setup, find_packages
